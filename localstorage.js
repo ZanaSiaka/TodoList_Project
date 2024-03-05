9aa7f77a3aeea9a7432e487bb3d2f39bdeb06ec6
ZanaSiaka <zanacoul21@gmail.com>
2024-03-05 02:07:08 +0000

    modif'Sauvegarde

--- localstorage.js
+++ localstorage.js
@@ -5,36 +5,67 @@ const input = document.getElementById("input-box");
 const btn = document.getElementById("btn");
 const conteneur = document.getElementById("conteneur-liste");
 
+
 form.addEventListener("submit", function(event){
-    if (input.value === ''){
+    if (input.value.trim() === ''){
         alert("Vous devez remplir le champ")
     }else{
         const li = document.createElement("li");
-        li.innerHTML = input.value;
+        li.innerHTML = input.value.trim();
         conteneur.appendChild(li);
         const span = document.createElement("span");
         span.innerHTML = "X";
         li.appendChild(span)
     }
 
+    if (input.value === ' '){
+        input.value = dad
+    }
+
+
     event.preventDefault();
     input.value = ''
     todolist()
+    verifierTache()
 })
 
 conteneur.addEventListener("click", function(e){
     if(e.target.tagName === 'SPAN'){
         e.target.parentElement.remove()
         todolist()
+        verifierTache()
     }
+    verifierTache()
 }, false)
 
 function todolist(){
-    localStorage.setItem("données", conteneur.innerHTML)
+    localStorage.setItem("tache", conteneur.innerHTML)
+    
 }
 
 function afficher(){
-    conteneur.innerHTML = localStorage.getItem("données")
+    // const li = document.createElement('li')
+    // li.textContent = localStorage.getItem("tache")
+    // const span = document.createElement('span')
+    // span.textContent = "X"
+    // li.appendChild(span)
+    // conteneur.appendChild(li)
+    conteneur.innerHTML = localStorage.getItem("tache")
 }
+
+function verifierTache(){
+    const noTache = document.querySelector("p")
+    if (!localStorage.getItem("tache") || localStorage.getItem("tache") === ''){
+        console.log('0')
+        noTache.style.display = "flex"
+    }else{
+        console.log('Zana')
+        noTache.style.display = "none"
+        console.log(localStorage.length)
+    }
+}
+
+
+verifierTache()
 
 afficher()
